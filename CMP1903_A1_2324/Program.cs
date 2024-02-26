4b7837afecc1428fbfce5724556249c87f943dcf
Dylan <158075286+d-hmlton@users.noreply.github.com>
2024-02-26 14:03:34 +0000

    A lot - the Testing class for one thing
    Made the Testing class, added XML comments, changes some default values in Die and Game, changes the curly brace formatting to be something I like more, gave Game a new method to return individual Die object vals for the Testing class' benefit, added a method for the Die class to return the DieVal, made the Game class save the sum to a new property called Sum / _sum. Lots of stuff

--- CMP1903_A1_2324/Program.cs
+++ CMP1903_A1_2324/Program.cs
@@ -4,19 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CMP1903_A1_2324
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+namespace CMP1903_A1_2324 {
+    /// <summary>
+    /// Creates a Game and Testing object, and calls their central methods - .RollDie() and .ForTest().
+    /// </summary>
+    internal class Program {
+        static void Main(string[] args) {
             /*
              * Create a Game object and call its methods.
              * Create a Testing object to verify the output and operation of the other classes.
              */
 
+            //Creating new 'Game' and 'Testing' objects
             Game game = new Game();
+            Testing test = new Testing();
+
+            //
             game.RollDie();
+            Console.WriteLine("\n--Testing--");
+            test.ForTest();
         }
     }
 }
--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -1,13 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CMP1903_A1_2324
-{
-    internal class Testing
-    {
+namespace CMP1903_A1_2324 {
+    /// <summary>
+    /// Uses Debug.Assert() to perform a number of tests on the Game and Die classes.
+    /// </summary>
+    /// <remarks>
+    /// The first of these tests ensures the properties of both are correctly set to their default value, -1.
+    /// The result of the Game object's methods is then compared to their expected outputs. This means the dice rolls are
+    ///  checked to see if they're within 1-6, the sum to see if it's within 3-18, and the "Sum" property is validated by
+    ///  having the Testing class perform it's own sum calculation.
+    /// Similarly, the Die class is also checked to ensure the result is within 1-6, and the "DieVal" property is validated
+    ///  by comparing it to the result the Die class returned.
+    /// </remarks>
+    internal class Testing {
         /*
          * This class should test the Game and the Die class.
          * Create a Game object, call the methods and compare their output to expected output.
@@ -15,7 +25,40 @@ namespace CMP1903_A1_2324
          * Use debug.assert() to make the comparisons and tests.
          */
 
+        //Properties
+        Game tesGame = new Game();
+        Die tesDie = new Die();
+        int tesSum = 0; int tesDieVal = 0;
+
         //Method
-        //
+        public void ForTest() {
+            //Ensuring the default value of the Game object's "Sum" property is -1
+            Debug.Assert(tesGame.Sum == -1);
+
+            //Ensuring the default value of the Die object's "DieVal" property is -1
+            Debug.Assert(tesDie.DieVal == -1);
+            
+            //Rolling die in our Game test object
+            tesGame.RollDie();
+
+            //Verifying the individual die values of the Game object
+            for (int i = 1; i < 4; i++) {
+                Debug.Assert(tesGame.GetRolls(i) <= 6 || tesGame.GetRolls(i) >= 1);
+                tesSum += tesGame.GetRolls(i); //Will be used later to test the sum
+            }
+
+            //Verifying the sum of the three die values of the Game object
+            Debug.Assert(tesGame.Sum <= 18 || tesGame.Sum >= 3);
+
+            //Verifying that the "Sum" given by the Game object is accurate
+            Debug.Assert(tesGame.Sum == tesSum);
+
+            //Verifying the output of the Die object
+            tesDieVal = tesDie.Roll();
+            Debug.Assert(tesDieVal <= 6 || tesDieVal >= 1);
+
+            //Verifying that the "DieVal" given by the Die object is accurate
+            Debug.Assert(tesDie.DieVal == tesDieVal);
+        }
     }
 }
