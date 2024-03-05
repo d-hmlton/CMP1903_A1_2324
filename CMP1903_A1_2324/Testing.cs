af4a13c6a8af38e600a44df235a41ea9a3a9c6a5
Dylan <158075286+d-hmlton@users.noreply.github.com>
2024-03-05 12:07:23 +0000

    Responding to review feedback (2) + style change
    Trimmed the <remarks> XML comment to be less overlong and repetitive, as suggested by Caitlin
    Spaced out a line with two operations to be two lines in improve readability, as suggested by Caitlin
    Changed the "tes(...)" objects to instead be "test(...)" to reduce potential confusion, as suggested by Wiktor
    Also made the int properties here protected and added an underscore to be better in line with style guide and expected code standards

--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -10,12 +10,10 @@ namespace CMP1903_A1_2324 {
     /// Uses Debug.Assert() to perform a number of tests on the Game and Die classes.
     /// </summary>
     /// <remarks>
-    /// The first of these tests ensures the properties of both are correctly set to their default value, -1.
-    /// The result of the Game object's methods is then compared to their expected outputs. This means the dice rolls are
-    ///  checked to see if they're within 1-6, the sum to see if it's within 3-18, and the "Sum" property is validated by
-    ///  having the Testing class perform it's own sum calculation.
-    /// Similarly, the Die class is also checked to ensure the result is within 1-6, and the "DieVal" property is validated
-    ///  by comparing it to the result the Die class returned.
+    /// Ensures the properties of both are correctly set to their default value, -1.
+    /// The result of the Game object's methods is compared to their expected outputs: dice rolls within 1-6, sum within 3-18, 
+    ///  and "Sum" property then validated.
+    /// The Die class is checked separately to ensure result is within 1-6. "DieVal" property is then validated.
     /// </remarks>
     internal class Testing {
         /*
@@ -26,39 +24,40 @@ namespace CMP1903_A1_2324 {
          */
 
         //Properties
-        Game tesGame = new Game();
-        Die tesDie = new Die();
-        int tesSum = 0; int tesDieVal = 0;
+        Game testGame = new Game();
+        Die testDie = new Die();
+        protected int _testSum = 0;
+        protected int _testDieVal = 0;
 
         //Method
         public void ForTest() {
             //Ensuring the default value of the Game object's "Sum" property is -1
-            Debug.Assert(tesGame.Sum == -1);
+            Debug.Assert(testGame.Sum == -1);
 
             //Ensuring the default value of the Die object's "DieVal" property is -1
-            Debug.Assert(tesDie.DieVal == -1);
+            Debug.Assert(testDie.DieVal == -1);
             
             //Rolling die in our Game test object
-            tesGame.RollDie();
+            testGame.RollDie();
 
             //Verifying the individual die values of the Game object
             for (int i = 1; i < 4; i++) {
-                Debug.Assert(tesGame.GetRolls(i) <= 6 || tesGame.GetRolls(i) >= 1);
-                tesSum += tesGame.GetRolls(i); //Will be used later to test the sum
+                Debug.Assert(testGame.GetRolls(i) <= 6 || testGame.GetRolls(i) >= 1);
+                _testSum += testGame.GetRolls(i); //Will be used later to test the sum
             }
 
             //Verifying the sum of the three die values of the Game object
-            Debug.Assert(tesGame.Sum <= 18 || tesGame.Sum >= 3);
+            Debug.Assert(testGame.Sum <= 18 || testGame.Sum >= 3);
 
             //Verifying that the "Sum" given by the Game object is accurate
-            Debug.Assert(tesGame.Sum == tesSum);
+            Debug.Assert(testGame.Sum == _testSum);
 
             //Verifying the output of the Die object
-            tesDieVal = tesDie.Roll();
-            Debug.Assert(tesDieVal <= 6 || tesDieVal >= 1);
+            _testDieVal = testDie.Roll();
+            Debug.Assert(_testDieVal <= 6 || _testDieVal >= 1);
 
             //Verifying that the "DieVal" given by the Die object is accurate
-            Debug.Assert(tesDie.DieVal == tesDieVal);
+            Debug.Assert(testDie.DieVal == _testDieVal);
         }
     }
 }
